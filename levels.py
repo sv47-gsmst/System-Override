41a8c31a9b8f50a2454d40ed19be946295d4e7d1
sv47-gsmst <s.vijayamanikandan47@gmail.com>
2026-01-31 20:47:39 -0500

    Create levels.py

---
+++ levels.py
@@ -0,0 +1,238 @@
+# Legend: W = Wall, P = Player, S = Switch, D = Door, E = Exit, . = Empty
+
+level1 = [
+    "WWWWWWWW",
+    "W..S.P.W",
+    "W..D..EW",
+    "WWWWWWWW"
+]
+
+level2 = [
+    "WWWWWWWWW",
+    "W....D..EW",
+    "W..WWW.P.W",
+    "W.S......W",
+    "WWWWWWWWW"
+]
+
+level3 = [
+    "WWWWWWWWW",
+    "WP....D..W",
+    "WWW.WW.W.W",
+    "W...S....W",
+    "W....WWW.W",
+    "W....E...W",
+    "WWWWWWWWW"
+]
+
+level4 = [
+    "WWWWWWWWWWWWW",
+    "W..S.......W",
+    "W..D..WWW..W",
+    "W.P....D..EW",
+    "WWWWWWWWWWWW"
+]
+
+levels = [level1, level2, level3, level4]
+
+# Additional Levels!
+level5 = [
+    "WWWWWWWWWWW",
+    "W....D....W",
+    "W..S.W.W..W",
+    "W.P....S.EW",
+    "WWWWWWWWWWW"
+]
+
+level6 = [
+    "WWWWWWWWWWWW",
+    "W.P..S.....W",
+    "W..WWW.WD..W",
+    "W....S.....W",
+    "W..D....E..W",
+    "WWWWWWWWWWWW"
+]
+
+level7 = [
+    "WWWWWWWWWWWWW",
+    "W..S...D....W",
+    "W.WWW.WWW.W.W",
+    "W.P.....S..EW",
+    "W..D...WWW..W",
+    "WWWWWWWWWWWWW"
+]
+
+level8 = [
+    "WWWWWWWWWWWWWW",
+    "W.P.....D...EW",
+    "W.WWWW.WWWWWW",
+    "W..S.....S..W",
+    "W..D.WWWW...W",
+    "WWWWWWWWWWWWWW"
+]
+
+level9 = [
+    "WWWWWWWWWWWWW",
+    "W.S.....D...W",
+    "W.WWW.WWW.W.W",
+    "W.P.....S..EW",
+    "W..D...WWW..W",
+    "WWWWWWWWWWWWW"
+]
+
+level10 = [
+    "WWWWWWWWWWWWWW",
+    "W.P...S....EW",
+    "W..WWW.D.W..W",
+    "W....S......W",
+    "W..D.WWWW...W",
+    "WWWWWWWWWWWWWW"
+]
+
+level11 = [
+    "WWWWWWWWWWWWWW",
+    "W.P.D.....S.EW",
+    "W..WWW.WWWW..W",
+    "W....S.......W",
+    "W..D...WWW...W",
+    "WWWWWWWWWWWWWW"
+]
+
+level12 = [
+    "WWWWWWWWWWWWWW",
+    "W..S...D.....W",
+    "W..WWW.WWWWW.W",
+    "W.P.....S..EW",
+    "W..D...WWW...W",
+    "WWWWWWWWWWWWWW"
+]
+
+level13 = [
+    "WWWWWWWWWWWWWW",
+    "W.P.....D....W",
+    "W..S.W.W.WS..W",
+    "W...S......E.W",
+    "WWWWWWWWWWWWWW"
+]
+
+level14 = [
+    "WWWWWWWWWWWWWW",
+    "W.P...S..D...W",
+    "W..WWW.WWWW..W",
+    "W....S.....EW",
+    "W..D...WWW...W",
+    "WWWWWWWWWWWWWW"
+]
+
+level15 = [
+    "WWWWWWWWWWWWWWW",
+    "W.P.....S.D..EW",
+    "W..WWW.WWWW...W",
+    "W....S.......W",
+    "W..D...WWW....W",
+    "WWWWWWWWWWWWWWW"
+]
+
+levels += [
+    level5, level6, level7, level8, level9, level10, level11, level12, level13, level14, level15
+]
+
+level16 = [
+    "WWWWWWWWWWWW",
+    "W.P...F..D.W",
+    "W..WWW.WWWW",
+    "W.S....S..EW",
+    "WWWWWWWWWWWW"
+]
+
+level17 = [
+    "WWWWWWWWWWWWW",
+    "W..S..D..F..W",
+    "W.WWWW.WWW.W",
+    "W.P....S...EW",
+    "W..D.......W",
+    "WWWWWWWWWWWWW"
+]
+
+level18 = [
+    "WWWWWWWWWWWW",
+    "W.P..S..F..W",
+    "W..D..WWW..W",
+    "W..S......EW",
+    "WWWWWWWWWWWW"
+]
+
+level19 = [
+    "WWWWWWWWWWWWWW",
+    "W..S.....D..W",
+    "W.FWWW.WWWW.W",
+    "W.P..F..S..EW",
+    "W..D......F.W",
+    "WWWWWWWWWWWWWW"
+]
+
+level20 = [
+    "WWWWWWWWWWWW",
+    "W.P.S.F..EW",
+    "W..WWW.W.WW",
+    "W..D..S...W",
+    "WWWWWWWWWWWW"
+]
+
+level21 = [
+    "WWWWWWWWWWWWW",
+    "W..S....D..W",
+    "W.FW.F.WWW.W",
+    "W.P..S....EW",
+    "W..D..F..W.W",
+    "WWWWWWWWWWWWW"
+]
+
+level22 = [
+    "WWWWWWWWWWWW",
+    "W.P..S..D.W",
+    "W..WWW.F..W",
+    "W..S....EW",
+    "W.F....F..W",
+    "WWWWWWWWWWWW"
+]
+
+level23 = [
+    "WWWWWWWWWWWWWWW",
+    "W.P.F.S..D...EW",
+    "W..WWW.WWWW...W",
+    "W....S.....F..W",
+    "W..D...WWW.....W",
+    "WWWWWWWWWWWWWWW"
+]
+
+level24 = [
+    "WWWWWWWWWWWW",
+    "W.P..S..F..EW",
+    "W..WWW.D.W..W",
+    "W..S.F.....W",
+    "W..D..WWW..W",
+    "WWWWWWWWWWWW"
+]
+
+level25 = [
+    "WWWWWWWWWWWWWW",
+    "W.P...S..F..EW",
+    "W..WWW.FWWW..W",
+    "W....S..D....W",
+    "W.F..D..WWW..W",
+    "WWWWWWWWWWWWWW"
+]
+
+level26 = [
+    "WWWWWWWWWWWWWW",
+    "W..S..D..F..W",
+    "W.P.F....S..EW",
+    "W..WWW.WWWW..W",
+    "W..D...F..E..W",
+    "WWWWWWWWWWWWWW"
+]
+
+levels += [
+    level16, level17, level18, level19, level20, level21, level22, level23, level24, level25, level26
+]
